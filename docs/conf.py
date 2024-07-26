c8624053f82f0bb15775aaa8199bf4bb092de2c4
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2024-07-27 02:09:42 +0300

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,63 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+import os, re
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+
+# -- Project information -----------------------------------------------------
+def getVersion_init():
+    init_path = os.path.join(os.getcwd(), "libdb", "__init__.py")
+    with open(init_path, 'rb') as init_py:
+        src = init_py.read().decode('utf-8')
+        ver = re.search("__version__ = ['\"]([^'\"]+)['\"]", src).group(1)
+        if not ver:
+            ver = re.search('__version__ = ["\']([^"\']+)["\']', src).group(1)
+        return f"{ver}"
+
+
+project = 'libdb'
+copyright = '2024, Mmdrza'
+author = 'Mmdrza'
+
+# The full version, including alpha/beta/rc tags
+ver = getVersion_init()
+release = f"'{ver}'"
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
